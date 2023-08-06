819441656e1028004195b4e4e5a798a30a19a84f
Gábor Farkas <farkas.gz@gmail.com>
2023-08-06 16:24:53 +0200

    catching up

--- back/models/User.model.js
+++ back/models/User.model.js
@@ -3,6 +3,12 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -1,41 +1,64 @@
 const router = require('express').Router()
 const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs")
+const User = require('../models/User.model')
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
+  const payload = req.body
 
   /* Hash the password using bcryptjs */
+  const salt = bcrypt.genSaltSync(13)
+  const passwordsHash = bcrypt.hashSync(payload.password, salt)
 
   /* Record your user to the DB */
 
-  res.json('Pinging signup')
+  try {
+    await User.create({username: payload.username, email: payload.email, password: passwordsHash})
+    res.status(201).json({message: "User created"})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 })
 
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
+  const payload = req.body
 
   /* Try to get your user from the DB */
+  const potentialUser = await User.findOne({username: payload.username})
+  console.log(potentialUser)
 
   /* If your user exists, check if the password is correct */
+  if(potentialUser) {
+    const passwordCheck = bcrypt.compareSync(payload.password, potentialUser.password)
+    console.log('usercheck: ', passwordCheck)
 
-  /* If your password is correct, sign the JWT using jsonwebtoken */
-  const authToken = jwt.sign(
-    {
-      expiresIn: '6h',
-      user: null, // Put yhe data of your user in there
-    },
-    process.env.TOKEN_SECRET,
-    {
-      algorithm: 'HS256',
+    if(passwordCheck){
+      /* If your password is correct, sign the JWT using jsonwebtoken */
+      const authToken = jwt.sign(
+        {userId: potentialUser._id},
+        process.env.TOKEN_SECRET,
+        {
+          algorithm: 'HS256',
+          expiresIn: '6h',
+        }
+      )
+      res.status(202).json({token: authToken})
+    } else {
+      res.status(403).json({errorMessage: 'Password invalid'})
     }
-  )
 
-  res.json('Pinging login')
+  } else {
+    res.status(403).json({errorMessage: 'Password invalid'})
+  }
 })
 
-router.get('/verify', (req, res, next) => {
+router.get('/verify', async (req, res, next) => {
   // You need to use the middleware there, if the request passes the middleware, it means your token is good
-  res.json('Pinging verify')
+  
+
 })
 
 module.exports = router
